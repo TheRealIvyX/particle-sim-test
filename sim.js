64127a65544237b3a0fae92c4f9e7eb4690815c8
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-01-09 19:04:19 +0100

    Update sim.js

--- sim.js
+++ sim.js
@@ -122,7 +122,7 @@ class Particle {
             }
           }
           if (dist(me, other) < 500) { // substitution in molecules
-            if (other.substitutionPriority < me.substitutionPriotity && (1-(Math.random()*Math.random()*Math.random())<(me.reactivity+other.reactivity)/2) && me.bonds.length < me.maxBonds+1) {
+            if (other.substitutionPriority < me.substitutionPriority && (1-(Math.random()*Math.random()*Math.random()))<(me.reactivity+other.reactivity)/2 && Math.random() <= 1/60) {
               if (me.bonds.length > 0) {
                 let swap = me.bonds[Math.floor(me.bonds.length*Math.random())]
                 me.splitBond(me, swap)
