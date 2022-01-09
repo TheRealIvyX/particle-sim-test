d29f69d818b4b705731cf4237fdd5196470f75ac
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-01-09 02:34:29 +0100

    Update sim.js

--- sim.js
+++ sim.js
@@ -61,14 +61,14 @@ class Particle {
   interact() {
     particles.forEach(function(other){
       if (other != this) {
-        if (dist(this, other) < 3000) { // attract particles to each other
+        /*if (dist(this, other) < 3000) { // attract particles to each other
           let force = 500
           console.log(force)
           force = force / dist(this, other) / dist(this, other)
           this.x += ((this.x-other.x)/dist(this, other))*force
           this.y += ((this.y-other.y)/dist(this, other))*force
         }
-      }
+      }*/
     })
   }
   draw() {
